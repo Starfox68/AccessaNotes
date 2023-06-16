3a79058202f0dfc1728e39ee6946b55e5b9b5cea
Henry <henryyu0198@gmail.com>
2023-06-16 16:47:36 -0400

    Setup Hilt Dependency Injection

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/NoteRepository.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/NoteRepository.kt
@@ -2,11 +2,14 @@ package com.shaphr.accessanotes.data
 
 import com.shaphr.accessanotes.data.sources.NoteSource
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
 
 
-class NoteRepository(
+@Singleton
+class NoteRepository @Inject constructor(
     private val noteSource: NoteSource
 ) {
     val favoriteLatestNews: Flow<Note> =
-            noteSource.noteFlow
+        noteSource.noteFlow
 }
--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/sources/NoteSource.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/sources/NoteSource.kt
@@ -3,8 +3,9 @@ package com.shaphr.accessanotes.data.sources
 import com.shaphr.accessanotes.data.Note
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class NoteSource {
+class NoteSource @Inject constructor() {
     var isSessionStarted: Boolean = false
     val noteFlow: Flow<Note> = flow {
         emit(Note("TEST STRING"))
