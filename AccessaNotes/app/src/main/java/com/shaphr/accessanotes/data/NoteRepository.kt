fbb806b4aff1f1a41e7c143e164786cd64985232
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-06-16 16:22:49 -0400

    Update NoteRepository.kt

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/NoteRepository.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/NoteRepository.kt
@@ -1,9 +1,12 @@
 package com.shaphr.accessanotes.data
 
-import java.util.concurrent.Flow
+import com.shaphr.accessanotes.data.sources.NoteSource
+import kotlinx.coroutines.flow.Flow
+
 
 class NoteRepository(
     private val noteSource: NoteSource
 ) {
-
+    val favoriteLatestNews: Flow<Note> =
+            noteSource.noteFlow
 }
