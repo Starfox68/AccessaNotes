1edc81ab40e3416b96c36f9d1a4536e10018bce0
Raymond Zhou <raymondzhou2019@gmail.com>
2023-06-25 10:42:18 -0400

    Add streaming

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/OpenAIAPIClient.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/OpenAIAPIClient.kt
@@ -1,24 +1,37 @@
 package com.shaphr.accessanotes
 
-import com.aallam.openai.api.edits.EditsRequest
+import com.aallam.openai.api.BetaOpenAI
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 
-class OpenAIAPIClient(private val modelName: String = "text-davinci-edit-001") {
+class OpenAIAPIClient(private val modelName: String = "gpt-3.5-turbo") {
     private val token = BuildConfig.OPENAI_API_KEY // Value needs to be set in local.properties
     private val openAIClient = OpenAI(token)
-    private val instruction = "Summarize the given text into notes using bullet points"
+    private val instruction = "Summarize the given text into notes using nested bullet points:\n"
 
-    fun summarize(prompt: String): String = runBlocking {
-        val edit = openAIClient.edit(
-            request = EditsRequest(
-                model = ModelId(modelName),
-                input = prompt,
-                instruction = instruction
+    @OptIn(BetaOpenAI::class)
+    fun summarize(prompt: String)  = runBlocking {
+        val request = ChatCompletionRequest(
+            model = ModelId(modelName),
+            messages = listOf(
+                ChatMessage(
+                    role = ChatRole.User,
+                    content = instruction + prompt
+                )
             )
         )
 
-        return@runBlocking edit.choices[0].text
+        openAIClient.chatCompletions(request)
+            .onEach { print(it.choices.first().delta?.content.orEmpty()) }
+            .onCompletion { println() }
+            .launchIn(this)
+            .join()
     }
 }
