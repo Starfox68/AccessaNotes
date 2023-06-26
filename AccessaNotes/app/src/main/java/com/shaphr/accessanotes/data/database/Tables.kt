e7bdd2c72d705e1a561b0f5e41009405bb6356a6
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-06-26 14:47:09 -0400

    SQL table setup for local room

---
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/Tables.kt
@@ -0,0 +1,21 @@
+package com.shaphr.accessanotes.data.database
+
+import java.io.Serializable
+import java.util.Date
+import androidx.room.*
+import kotlinx.serialization.json.JsonNames
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Entity(
+    tableName = "Note",
+)
+data class Note(
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "date") val notifyAt: Long = Date().time,
+//    @ColumnInfo(name = "folderId") val folderId: Long?,
+    @ColumnInfo(name = "createdAt") val createdAt: Long = Date().time,
+    @ColumnInfo(name = "updatedAt") val updatedAt: Long = Date().time,
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+) : Serializable
