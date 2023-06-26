632eb65a4b8f3c05ee0238ce70d97497a4632e27
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-06-26 14:57:21 -0400

    NoteDatabase.kt set up

---
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/NoteDatabase.kt
@@ -0,0 +1,55 @@
+package com.shaphr.accessanotes.data.database
+
+import android.content.Context
+import androidx.room.*
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+@Database(entities = [Note::class], version = 18)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun getNoteDataAccess(): NoteDataAccess
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "note_database"
+                )
+                    .fallbackToDestructiveMigration() // increment version number with losing data. must be removed and replaced with proper migrations
+                    .addCallback(CALLBACK)
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+        private val CALLBACK = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+
+                val tables = arrayOf("Note")
+
+                tables.forEach {
+                    db.execSQL("CREATE TRIGGER IF NOT EXISTS LogDeletion$it " +
+                            "BEFORE DELETE ON $it FOR EACH ROW BEGIN " +
+                            "INSERT INTO DeleteLog (tableName, idPrimary, deletedAt) " +
+                            "VALUES ('$it', old.id, strftime('%s','now') || substr(strftime('%f','now'),4)); " +
+                            "END;"
+                    )
+                }
+
+                db.execSQL("CREATE TRIGGER IF NOT EXISTS LogDeletionTagNoteCrossRef " +
+                        "BEFORE DELETE ON TagNoteCrossRef FOR EACH ROW BEGIN " +
+                        "INSERT INTO DeleteLog (tableName, idPrimary, idSecondary, deletedAt) " +
+                        "VALUES ('TagNoteCrossRef', old.tagId, old.noteId, strftime('%s','now') || substr(strftime('%f','now'),4)); " +
+                        "END;"
+                )
+            }
+        }
+    }
+}
