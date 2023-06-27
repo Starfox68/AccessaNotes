51e1367f94906475ec9f623a0ad8a0c062938c64
Ayushi Tiwari <ayushitiwari@Ayushis-MacBook-Pro.local>
2023-06-27 12:10:23 -0400

    fix: optimized imports in NoteRepositoryScreen

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/screens/NoteRepositoryScreen.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/screens/NoteRepositoryScreen.kt
@@ -7,16 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
