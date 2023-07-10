31ab53df3aa01712414d5e2625873ec9ee267f81
Raymond Zhou <raymondzhou2019@gmail.com>
2023-07-10 17:44:21 -0400

    Add dynamic pdf height

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/FileCompiler.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/FileCompiler.kt
@@ -1,10 +1,8 @@
 package com.shaphr.accessanotes
 
-import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.graphics.pdf.PdfDocument
-import android.graphics.pdf.PdfDocument.Page
 import android.graphics.pdf.PdfDocument.PageInfo
 import android.os.Environment
 import android.text.StaticLayout
@@ -15,36 +13,46 @@ import java.io.IOException
 
 
 class FileCompiler {
+    private val psPerInch = 72
+    private val pageWidth = (9.5 * psPerInch).toInt()
+    private val margins = 1 * psPerInch
+    private val filePath = "${Environment.getExternalStorageDirectory()}/Download"
 
-    fun toPDF(title: String, text: String): PdfDocument {
-        val doc = PdfDocument()
-        val page = doc.startPage(PageInfo.Builder(684, 792, 1).create())
-
-        val canvas = page.canvas
+    fun getPDF(title: String, text: String): PdfDocument {
         val titlePaint = TextPaint()
-        val bodyPaint = TextPaint()
-
         titlePaint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
         titlePaint.textSize = 24F
         titlePaint.textAlign = Paint.Align.CENTER
 
-        canvas.drawText(title, canvas.width.toFloat()/2, 98F, titlePaint)
-        canvas.translate(72F, 114F)
-
+        val bodyPaint = TextPaint()
         bodyPaint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
         bodyPaint.textSize = 16F
 
-        val sb = StaticLayout.Builder.obtain(text, 0, text.length, bodyPaint,
-            canvas.width-144).setLineSpacing(2F, 1F)
-        sb.build().draw(canvas)
+        val layout = StaticLayout.Builder.obtain(
+            text, 0, text.length, bodyPaint, pageWidth - 2 * margins
+        ).setLineSpacing(2F, 1F).build()
+
+        val doc = PdfDocument()
+        val page = doc.startPage(
+            PageInfo.Builder(
+                pageWidth, (layout.height + 2.5 * margins + titlePaint.textSize).toInt(),
+                1
+            ).create()
+        )
+        val canvas = page.canvas
+
+        // Draw title at centre of canvas
+        canvas.drawText(title, canvas.width / 2F, 1.5F * margins, titlePaint)
+        // Draw text below title
+        canvas.translate(1F * margins, 1.5F * margins + titlePaint.textSize)
+        layout.draw(canvas)
 
         doc.finishPage(page)
         return doc
     }
 
-    fun writePDF(doc: PdfDocument) {
-        val filePath = "${Environment.getExternalStorageDirectory()}/Download"
-        val file = File(filePath, "test.pdf")
+    fun writePDF(doc: PdfDocument, title: String) {
+        val file = File(filePath, "$title.pdf")
 
         try {
             println("Writing file to $filePath")
