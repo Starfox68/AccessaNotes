b3af88021704899df621a72b06d920c31eb74a17
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-07-14 18:49:24 -0400

    note data fixs

---
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/NoteDataAccess.kt
@@ -0,0 +1,24 @@
+package com.shaphr.accessanotes.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Dao
+interface NoteDataAccess {
+    @Insert(onConflict = OnConflictStrategy.IGNORE, entity = Note::class)
+    suspend fun insert(note: Note): Long
+
+    @Update(entity = Note::class)
+    suspend fun update(note: Note)
+
+    @Delete(entity = Note::class)
+    suspend fun delete(note: Note)
+
+    @Query("DELETE FROM `Note` WHERE id = :id")
+    fun deleteNoteById(id: Long)
+
+    @Query("SELECT * FROM `Note` ORDER BY id ASC")
+    fun getNotes(): LiveData<List<Note>>
+}
