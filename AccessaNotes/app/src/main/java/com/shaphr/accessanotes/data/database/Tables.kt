bf3b386ba93e7648a106cfc8a23a449da1518c5c
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-07-14 18:49:15 -0400

    create table

---
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/Tables.kt
@@ -0,0 +1,20 @@
+package com.shaphr.accessanotes.data.database
+
+import java.io.Serializable
+import java.util.Date
+import androidx.room.*
+import kotlinx.serialization.json.JsonNames
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
+
+@Entity(
+    tableName = "Note",
+)
+data class Note(
+    @ColumnInfo(name = "title") val title: String = "",
+    @ColumnInfo(name = "content") val content: String = "",
+    @ColumnInfo(name = "date") val date: LocalDate? = null,
+    @ColumnInfo(name = "date") val notifyAt: Long = Date().time,
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+) : Serializable
