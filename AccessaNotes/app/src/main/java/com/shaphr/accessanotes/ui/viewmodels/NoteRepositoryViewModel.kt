d4a453b282389eba83dd28908f65cacdcd1969c4
Raymond Zhou <raymondzhou2019@gmail.com>
2023-07-17 15:26:22 -0400

    Add tts to single note screen

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/viewmodels/NoteRepositoryViewModel.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/viewmodels/NoteRepositoryViewModel.kt
@@ -1,6 +1,7 @@
 package com.shaphr.accessanotes.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.shaphr.accessanotes.TextToSpeechClient
 import com.shaphr.accessanotes.data.models.Note
 import com.shaphr.accessanotes.data.repositories.NotesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,11 +11,24 @@ import javax.inject.Inject
 
 @HiltViewModel
 class NoteRepositoryViewModel @Inject constructor(
-    private val notesRepository: NotesRepository
+    private val notesRepository: NotesRepository,
+    private val textToSpeechClient: TextToSpeechClient
 ) : ViewModel() {
 
     private val mutableNotes: MutableStateFlow<List<Note>> = MutableStateFlow(notesRepository.getNotes())
     val notes: StateFlow<List<Note>> = mutableNotes
+    // Track if tts currently speaking
+    var isSpeaking = false
 
     fun getNote(id: Int) = notesRepository.getNotes().first { it.id == id }
+
+    fun onTextToSpeech(text: String) {
+        if (!isSpeaking) {
+            textToSpeechClient.speak(text)
+        } else {
+            textToSpeechClient.stop()
+        }
+
+        isSpeaking = !isSpeaking
+    }
 }
