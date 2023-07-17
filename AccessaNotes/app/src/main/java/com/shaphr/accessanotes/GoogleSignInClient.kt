66744af3125294c3fd2ece5be5526f3f72018690
Ayushi Tiwari <ayushitiwari2103@gmail.com>
2023-07-17 13:17:38 -0400

    fix: added googleSignIn client

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/GoogleSignInClient.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/GoogleSignInClient.kt
@@ -1,2 +1,16 @@
 package com.shaphr.accessanotes
 
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+    val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//         Request id token if you intend to verify google user from your backend server
+//        .requestIdToken(context.getString(R.string.backend_client_id))
+        .requestEmail()
+        .build()
+
+    return GoogleSignIn.getClient(context, signInOptions)
+}
