52178831f82eb2033605694a5617f1835ccc0f39
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-07-18 17:51:28 -0400

    converter for local date time

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/NoteDatabase.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/NoteDatabase.kt
@@ -5,6 +5,25 @@ import androidx.room.*
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 
+import androidx.room.TypeConverter
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): LocalDate? {
+        return value?.let {
+            Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault()).toLocalDate()
+        }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: LocalDate?): Long? {
+        return date?.atStartOfDay(ZoneId.systemDefault())?.toInstant()?.toEpochMilli()
+    }
+}
+
 @Database(entities = [Note::class], version = 18)
 @TypeConverters(Converters::class)
 abstract class NoteDatabase : RoomDatabase() {
--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/Tables.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/database/Tables.kt
@@ -9,19 +9,6 @@ import java.time.LocalDate
 import java.util.*
 import androidx.room.TypeConverter
 
-class Converters {
-    @TypeConverter
-    fun fromTimestamp(value: String?): LocalDate? {
-        return value?.let { LocalDate.parse(it) }
-    }
-
-    @TypeConverter
-    fun dateToTimestamp(date: LocalDate?): String? {
-        return date?.toString()
-    }
-}
-
-
 @Entity(
     tableName = "Note",
 )
