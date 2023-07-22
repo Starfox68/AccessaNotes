6b659484c3c3a6da858a35aa0d7f9af0d6bc5648
Parth More <parthmore0116@gmail.com>
2023-07-22 12:25:37 -0400

    rough outline for note repository

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/screens/NoteRepositoryScreen.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/screens/NoteRepositoryScreen.kt
@@ -3,6 +3,8 @@ package com.shaphr.accessanotes.ui.screens
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,7 +16,9 @@ import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -36,6 +40,7 @@ import com.shaphr.accessanotes.ui.viewmodels.NoteRepositoryViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import com.shaphr.accessanotes.data.database.Note
 import com.shaphr.accessanotes.ui.components.BottomNavBar
 
@@ -60,12 +65,10 @@ fun NoteRepositoryScreen(
     );
 
     Scaffold (
-//        topBar = { TopNav("All Saved Notes") },
         content = { padding ->
             LazyColumn(modifier = Modifier.padding(padding)) {
                 notes.forEach { note ->
                     item(note.id) {
-//                        Divider()
                         val paddingModifier = Modifier
                             .padding(10.dp)
                             .fillMaxWidth()
@@ -76,19 +79,22 @@ fun NoteRepositoryScreen(
 ////                                    )
 ////                                )
 //                            }
-                        Card(shape = RoundedCornerShape(20.dp), modifier = paddingModifier) {
-                            Column(modifier = paddingModifier) {
+                        Card(shape = RoundedCornerShape(3.dp), modifier = paddingModifier, elevation = CardDefaults.cardElevation(10.dp)) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Column() {
                                 Text(note.title, color = Color.Black)
-                                Text(note.content, color = Color.Gray)
+                                    Text(note.date.toString(), color = Color.Gray)
+                                }
+                                Spacer(modifier = Modifier.weight(1f))
+                                Button(onClick = { /*TODO*/ }) {
+                                    Text("Share to Drive")
+                                }
                             }
                         }
                     }
                 }
             }
         },
-//        floatingActionButton = {
-//            FloatingActionButton(navController)
-//        },
         bottomBar = {
             BottomNavBar(navController)
         }
