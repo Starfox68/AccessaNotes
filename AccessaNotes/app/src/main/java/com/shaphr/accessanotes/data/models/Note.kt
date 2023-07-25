2bb88e3dfb5c0dc2bedf053dc0808b0753e91884
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2023-07-25 14:52:27 -0400

    NoteUi Model

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/models/Note.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/data/models/Note.kt
@@ -1,10 +1,26 @@
 package com.shaphr.accessanotes.data.models
 
+import android.graphics.Bitmap
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import com.shaphr.accessanotes.data.database.NoteItem
 import java.time.LocalDate
+import java.util.Date
 
-//data class Note(
-//    val id: Int,
-//    val title: String = "",
-//    val date: LocalDate? = null,
-//    val content: String = ""
-//)
+data class UiNote(
+    val title: String = "",
+    val content: String = "",
+    val transcript: String = "",
+    val date: LocalDate? = null,
+    val notifyAt: Long = Date().time,
+    var id: Int = 0,
+    val items: List<UiNoteItem>? = null,
+)
+data class UiNoteItem(
+    val id: Int,
+    val noteId: Int,
+    val imageTrue: Boolean,
+    val content: String?,
+    val bitmap: Bitmap?,
+    val order: Int,
+)
