0f1839fc350731e7ecd4b217cdd1a41d3438e54a
Parth More <parthmore0116@gmail.com>
2023-07-26 14:38:56 -0400

    account settings auth button update

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/viewmodels/AccountSettingsViewModel.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/viewmodels/AccountSettingsViewModel.kt
@@ -1,10 +1,13 @@
 package com.shaphr.accessanotes.ui.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.shaphr.accessanotes.data.repositories.AccountSettingsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,6 +19,13 @@ class AccountSettingsViewModel @Inject constructor(
     val fontFlow: Flow<Boolean> = accountSettingsRepository.fontFlow
 
     val colourBlindFlow: Flow<Boolean> = accountSettingsRepository.colourFlow
+
+    private val mutableAuthenticated: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val authenticated: StateFlow<Boolean> = mutableAuthenticated
+
+    fun setAuthenticated(){
+        mutableAuthenticated.value = true
+    }
 
     fun onFontClick(isLarge: Boolean){
         viewModelScope.launch {
