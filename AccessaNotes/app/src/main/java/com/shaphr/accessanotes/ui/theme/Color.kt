13d32e1f1bfc8be31a9b96a981d6680def3ad9ae
Ayushi Tiwari <ayushitiwari2103@gmail.com>
2023-07-26 11:16:29 -0400

    fix: changed color

--- AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/theme/Color.kt
+++ AccessaNotes/app/src/main/java/com/shaphr/accessanotes/ui/theme/Color.kt
@@ -3,10 +3,10 @@ package com.shaphr.accessanotes.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleGrey80 = Color(0xF0427DDE)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF427DDE)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val PinkLight = Color(0xFFe8ddff)
